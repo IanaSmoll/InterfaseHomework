740ecfd00aea429b4d4c8e211d35fa5cd733264b
Iana Smolitskaia <113282768+IanaSmoll@users.noreply.github.com>
2022-11-25 08:41:35 +0000

    Update Converter.java

--- task1/Converter.java
+++ task1/Converter.java
@@ -1,33 +1,25 @@
 import Converter.CelsiusConvereter;
 
-interface Converter
-{
+interface Converter{
     public double getConvertedValue(double baseTemperature);
 }
 
-    class CelsiusConverter implements Converter
-    {
+    class CelsiusConverter implements Converter{
         @Override
-        public double getConvertedValue(double baseTemperature)
-        {
+        public double getConvertedValue(double baseTemperature){
             return baseTemperature;
         }
     }
-
-    class KelvinConverter implements Converter
-    {
+    class KelvinConverter implements Converter{
         @Override
-        public double getConvertedValue(double baseTemperature)
-        {
+        public double getConvertedValue(double baseTemperature){
             return baseTemperature + 273.15;
         }
     }
 
-    class FahrenheitConverter implements Converter
-    {
+    class FahrenheitConverter implements Converter{
         @Override
-        public double getConvertedValue(double baseTemperature)
-        {
+        public double getConvertedValue(double baseTemperature) {
             return 1.8 * baseTemperature + 32;
         }
     }
